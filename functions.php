8d0022ec1dd298714509e2eddc6b8a26ce4b58d5
alex-menin <alex.menin11@gmail.com>
2019-02-18 11:38:41 -0300

    Fixed bugs in name of files

--- functions.php
+++ functions.php
@@ -19,14 +19,14 @@ new_css('main', 'css/main.css');
 new_css('style', 'style.css');
 
 /*----------  Use CSS Default  ----------*/
-use_css('font-raleway');
-use_css('bootstrap');
-//use_css('fontawesome');
-//use_css('owl-carousel');
-//use_css('owl-theme');
-//use_css('lightgallery');
-//use_css('animate');
-//use_css('iziToast');
+// use_css('font-raleway');
+// use_css('bootstrap');
+// use_css('fontawesome');
+// use_css('owl-carousel');
+// use_css('owl-theme');
+// use_css('lightgallery');
+// use_css('animate');
+// use_css('iziToast');
 use_css('main');
 use_css('style');
 
@@ -35,7 +35,7 @@ use_css('style');
 # =           Script Files           =
 # ====================================
 /*----------  New JS Default  ----------*/
-new_js('modernizr', 'js/vendor/modernizr-3.5.0.min.js');
+new_js('modernizr', 'js/vendor/modernizr-3.5.0.min.js', true);
 new_js('jquery', 'js/vendor/jquery-3.3.1.min.js', true);
 new_js('popper', 'js/popper.min.js', true);
 new_js('bootstrap', 'js/bootstrap.min.js', true);
@@ -44,18 +44,18 @@ new_js('lightgallery', 'js/lightgallery.min.js', true);
 new_js('jquery.mask', 'js/jquery.mask.min.js', true);
 new_js('wow', 'js/wow.min.js', true);
 new_js('iziToast', 'js/iziToast.min.js', true);
-new_js('main', 'js/lazyload.min.js', true);
+new_js('lazyload', 'js/lazyload.min.js', true);
 new_js('main', 'js/main.js', true);
 
 /*----------  Use JS Default  ----------*/
-use_js('modernizr');
-use_js('jquery');
-use_js('popper');
-use_js('bootstrap');
-//use_js('owl-carousel');
-//use_js('lightgallery');
-//use_js('jquery.mask');
-//use_js('wow');
-//use_js('iziToast');
-//use_js('lazyload');
+// use_js('modernizr');
+// use_js('jquery');
+// use_js('popper');
+// use_js('bootstrap');
+// use_js('owl-carousel');
+// use_js('lightgallery');
+// use_js('jquery.mask');
+// use_js('wow');
+// use_js('iziToast');
+// use_js('lazyload');
 use_js('main');
