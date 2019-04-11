6b3024e3112d4bcf37b1cee7bc17032a4e9340fb
alex-menin <alex.menin11@gmail.com>
2019-04-11 16:15:18 -0300

    Ajusts

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 /* LOGO COMET
 <a href="https://comet.com.br/" target="_blank" title="Comet">
-    <img src="https://s3-sa-east-1.amazonaws.com/s3.dvulgsolucoes.com.br/assets/img/logo-desenvolvimento-dvulg.png" alt="Comet" height="45">
+    <img src="https://s3-sa-east-1.amazonaws.com/s3.dvulgsolucoes.com.br/assets/img/logo-desenvolvimento-dvulg.png" alt="Comet" height="35">
 </a>
 */ ?>
 
