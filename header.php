da9ec0b5cf02d274ec45f0a8252d405de2d1c5dc
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-03-02 13:42:40 -0300

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
 
 	<title><?php echo is_front_page() ? '' : wp_title('', false).' | '; bloginfo('name'); ?></title>
 
