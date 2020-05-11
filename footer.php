325b47f2d1a805f74d3e26f9b1b43a439709ef59
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-05-11 08:48:08 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <?php
 /* LOGO COMET
 <a href="https://comet.com.br/" target="_blank" title="Comet">
-    <img src="https://s3-sa-east-1.amazonaws.com/s3.dvulgsolucoes.com.br/assets/img/logo-desenvolvimento-dvulg.png" alt="Comet" height="35">
+    <img src="https://s3-sa-east-1.amazonaws.com/s3.dvulgsolucoes.com.br/assets/img/logo-desenvolvimento-dvulg.png" alt="Comet" height="30">
+</a>
+<a href="https://inova.agency" target="_blank" title="iNova">
+    <img src="https://assets.comet.com.br/comet/wordpress/2020/05/11084631/inova.png" alt="iNova" height="30">
 </a>
 */ ?>
 
