ebb2b03ed7ae6b19ee0bd835f5ba0c91b7d8ce64
Alexandre <alex.menin11@gmail.com>
2020-05-15 08:25:23 -0300

    feat: add tag HTML

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,7 @@
-<?php get_template_part('template-parts/sidebar/get_categories'); ?>
+<aside>
+  <?php get_template_part('template-parts/sidebar/get_categories'); ?>
 
-<?php get_template_part('template-parts/sidebar/get_tags'); ?>
+  <?php get_template_part('template-parts/sidebar/get_tags'); ?>
 
-<?php get_template_part('template-parts/sidebar/get_archives'); ?>
+  <?php get_template_part('template-parts/sidebar/get_archives'); ?>
+</aside>
