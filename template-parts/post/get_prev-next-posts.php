24a6f612d7cb5f1d20ba484a936adce5881d8b82
Alexandre Menin <43181420+alexmeninf@users.noreply.github.com>
2020-05-20 11:05:40 -0300

    Update get_prev-next-posts.php

--- template-parts/post/get_prev-next-posts.php
+++ template-parts/post/get_prev-next-posts.php
@@ -3,14 +3,18 @@ $previous = get_previous_post();
 $next     = get_next_post();
 ?>
 
-<div class="nav-previous">
+<div class="pagination-posts">
   <?php if ( get_previous_post() ) { ?>
-    <a href="<?= get_the_permalink($previous) ?>"><< Post anterior</a>
+    <a href="<?= get_the_permalink($previous) ?>" title="Lê postagem: <?= get_the_title($previous) ?>" class="nav-previous">
+      <i class="far fa-chevron-double-left"></i> 
+      <span>Anterior</span>
+    </a>
   <?php } ?>
-</div>
 
-<div class="nav-next">
   <?php if ( get_next_post() ) { ?>
-    <a href="<?= get_the_permalink($next) ?>">Próxima post >></a>
+    <a href="<?= get_the_permalink($next) ?>" title="Lê postagem: <?= get_the_title($next) ?>" class="nav-next">
+      <span>Próxima</span> 
+      <i class="far fa-chevron-double-right"></i>
+    </a>
   <?php } ?>
 </div>
