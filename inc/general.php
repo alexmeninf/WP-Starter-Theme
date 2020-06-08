b2a40ab1b71a957a02f614af305708a310306598
Alexandre <alex.menin11@gmail.com>
2020-06-08 11:04:34 -0300

    feat: update after setup theme

--- inc/general.php
+++ inc/general.php
@@ -2,20 +2,20 @@
 /**
  * Add HTML5 theme support.
  */
-function wpdocs_after_setup_theme() {
-  add_theme_support( 'html5', array( 'search-form' ) );
+function wp_after_setup_theme() {
+  add_theme_support( 'html5', array( 'search-form', 'style', 'script' ) );
 }
-add_action( 'after_setup_theme', 'wpdocs_after_setup_theme' );
+add_action( 'after_setup_theme', 'wp_after_setup_theme' );
 
 
 /**
  * UPDATE WORDPRESS LOGIN LOGO DARK
  */
-function my_login_logo() { ?>
+function wp_custom_logo_in_login() { ?>
 	<style type="text/css">
 	#login h1 a, .login h1 a {background-image:url('http://s3.dvulgsolucoes.com.br/assets/img/logo-dvulg-wordpress-white.png');background-repeat:no-repeat;height:105px;padding-bottom:30px;width:105px;}body{background:#222!important}.login #backtoblog a,.login #nav a{color:#adadad!important}.login #login_error,.login .message,.login .success,.login form{border-radius:10px}.wp-core-ui .button-primary{background:#eb6358!important;border-color:#eb6358!important;box-shadow:none!important;color:#fff!important;text-decoration:none!important;text-shadow:none!important}
 	</style>
-<?php } add_action( 'login_enqueue_scripts', 'my_login_logo' );
+<?php } add_action( 'login_enqueue_scripts', 'wp_custom_logo_in_login' );
 
 
 /**
