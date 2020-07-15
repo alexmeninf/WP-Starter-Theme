a887cb88d2e1f16e2d891baf41188be2caa3682b
Alexandre Menin <alex.menin11@gmail.com>
2020-07-15 09:32:44 -0300

    Update general.php

--- inc/general.php
+++ inc/general.php
@@ -39,9 +39,10 @@ add_action('wp_head', 'get_og_protocol', 2);
  * UPDATE WORDPRESS LOGIN LOGO DARK
  */
 function wp_custom_logo_in_login() { ?>
-	<style type="text/css">
-	#login h1 a, .login h1 a {background-image:url('http://s3.dvulgsolucoes.com.br/assets/img/logo-dvulg-wordpress-white.png');background-repeat:no-repeat;height:105px;padding-bottom:30px;width:105px;}body{background:#222!important}.login #backtoblog a,.login #nav a{color:#adadad!important}.login #login_error,.login .message,.login .success,.login form{border-radius:10px}.wp-core-ui .button-primary{background:#eb6358!important;border-color:#eb6358!important;box-shadow:none!important;color:#fff!important;text-decoration:none!important;text-shadow:none!important}
-	</style>
+<style type="text/css">
+#login h1 a, .login h1 a {background-image:url('https://comet-space.nyc3.digitaloceanspaces.com/assets/default/logo-inova-circle-pink.png');background-repeat:no-repeat;background-size: 120px;height:120px;width:120px;}body{background:#141414!important}.login #backtoblog a,.login #nav a{color:#adadad!important}.login #login_error,.login .message,.login .success,.login form{border-radius:10px}.wp-core-ui .button-primary{background:#000!important;border-color:#000!important;box-shadow:none!important;color:#fff!important;text-decoration:none!important;text-shadow:none!important;border-radius:0!important;}input[type=text]:focus, input[type=password]:focus {  border-color: #ff0083 !important;
+  box-shadow: none !important;}
+</style>
 <?php } add_action( 'login_enqueue_scripts', 'wp_custom_logo_in_login' );
 
 
