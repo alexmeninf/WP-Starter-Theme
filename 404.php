f011adcc13f8b30c88df90dd462b7641fbdf751b
Alexandre Menin <alex.menin11@gmail.com>
2020-10-29 09:49:47 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 text-center py-5">
-				<h1>404</h1>
+				<h1 class="title">404</h1>
 				<p>Desculpe, mas parece que a página que você acessou não existe ou pode ter sido removida.</p>
 				<a href="<?php bloginfo('url') ?>" class="btn btn-primary">Voltar ao Início</a>
 			</div>
