2a089881fd66df2e4c8ad6a5650bc01fa1d31eae
Alexandre Menin <alex.menin11@gmail.com>
2020-11-08 22:03:08 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,8 +1,11 @@
+<?php get_header(); ?>
+
+
 <div class="page-404 full spacing">
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-7 text-center py-5">
-				<h1 class="headline-lg wow fadeInUp">404</h1>
+				<h1 class="title wow fadeInUp">404</h1>
 				<p class="headline-sm wow fadeInUp" data-wow-delay=".2s">Página não encontrada</p>
 				<p class="wow fadeInUp" data-wow-delay=".3s">Desculpe, mas parece que a página que você acessou não existe ou pode ter sido removida.</p>
 				<a href="<?php bloginfo('url') ?>" class="btn wow fadeInUp" data-wow-delay=".4s"><span>Voltar ao Início</span></a>
@@ -10,3 +13,6 @@
 		</div>
 	</div>
 </div>
+
+
+<?php get_footer(); ?>
