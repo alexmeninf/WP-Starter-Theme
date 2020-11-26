f465f79b3a1c21da48164f4e2edd8b788b443eaa
alexmeninf <alex.menin11@gmail.com>
2020-11-26 19:54:27 -0200

    Feat: new social networks were added to share and improvements

--- template-parts/post/get_share-post.php
+++ template-parts/post/get_share-post.php
@@ -8,10 +8,13 @@
       <a href="https://twitter.com/intent/tweet?text=<?php the_title() ?>%2E%20notícia%20completa%20em%3A%20&tw_p=tweetbutton&url=<?php the_permalink() ?>" class="twitter-share-button" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a>
     </li>
     <li>
-      <a href="https://pinterest.com/pin/create/button/?url=<?php the_permalink() ?>&media=<?= has_post_thumbnail() ? wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "medium" )[0] : '' ?>&description=<?php the_title() ?>" target="_blank" rel="noopener"><i class="fab fa-pinterest-p"></i></a>
+      <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>&title=&summary=<?php the_title() ?>&source=" target="_blank" rel="noopener"><i class="fab fa-linkedin-in"></i></a>
     </li>
     <li>
-      <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>&title=&summary=<?php the_title() ?>&source=" target="_blank" rel="noopener"><i class="fab fa-linkedin-in"></i></a>
+      <a href="https://api.whatsapp.com/send?phone=&text=<?php the_title() ?>.%20Saiba%20mais%20em%3A%20<?php the_permalink() ?>" target="_blank" rel="noopener"><i class="fab fa-whatsapp"></i></a>
+    </li>
+    <li>
+      <a href="https://telegram.me/share/url?url=<?php the_permalink() ?>&text=<?= the_title() ?>" target="_blank" rel="noopener"><i class="fab fa-telegram-plane"></i></a>
     </li>
   </ul>
 </div>
