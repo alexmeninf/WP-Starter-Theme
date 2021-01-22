6eb0574f4fd0f44da682b010eba43961af161aed
Alexandre Menin <alex.menin11@gmail.com>
2021-01-22 16:14:57 -0300

    Update general.php

--- inc/general.php
+++ inc/general.php
@@ -215,3 +215,36 @@ function get_pagination($current_page, $pages_count, $maxLinks = 2) {
 		</nav>
 <?php endif;
 }
+
+
+ /**
+ * input
+ *
+ * @param  mixed $name
+ * @param  mixed $type
+ * @param  mixed $id
+ * @return void
+ */
+function input($name, $id, $type, $is_required = false, $value = '') { 
+	$value = (trim($value) != '') ? ' value="' .$value. '"' : '';
+	$required = $is_required ? 'required' : ''; 
+
+	if ($type == 'hidden') : ?>
+		<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?>>
+
+	<?php elseif ($type == 'textarea') : ?>
+		<label class="form-group">
+			<textarea id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;" <?= $required ?>></textarea>
+			<span class="txt"><?= $name ?> <sup>*</sup></span>
+			<span class="bar"></span>
+		</label>
+
+	<?php else: ?>
+		<label class="form-group">
+			<input type="<?= $type ?>" id="<?= $id ?>" name="<?= $id ?>"<?= $value ?> placeholder="&nbsp;" <?= $required ?>>
+			<span class="txt"><?= $name ?> <sup>*</sup></span>
+			<span class="bar"></span>
+		</label>
+<?php
+	endif;
+}
