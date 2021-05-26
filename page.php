e9e5501983922bbc3fcb1e90d9d77263d2f6aea3
Alexandre Menin <alex.menin11@gmail.com>
2021-05-26 16:04:03 -0300

    Update page.php

--- page.php
+++ page.php
@@ -3,18 +3,18 @@
 
 
 <section class="min-vh-100 d-flex align-items-center spacing">
-  <div class="container">
-    <div class="row">
+  <div class="container<?= is_active_sidebar( 'sidebar-2' ) ? '-fluid' : '' ?>">
+    <div class="row justify-content-center">
       
       <?php if ( is_active_sidebar( 'sidebar-2' )  ) : ?>
-        <div class="col-lg-4 widget-area" role="complementary">
+        <div class="col-md-4 col-lg-3 col-xxl-2 widget-area" role="complementary">
           
           <?php dynamic_sidebar( 'sidebar-2' ); ?>
           
         </div>
       <?php endif; ?>
       
-      <div class="col-lg-<?= is_active_sidebar( 'sidebar-2' ) ? '8' : '4' ?>">
+      <div class="<?= is_active_sidebar( 'sidebar-2' ) ? 'col-md-8 col-lg-9 col-xxl-10' : '10' ?>">
         <?php the_content(); ?>
       </div>
     </div>
