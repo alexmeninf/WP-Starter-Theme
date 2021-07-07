0ba112360366b8872e8b7bca651392d3440314d7
Alexandre Menin <alex.menin11@gmail.com>
2021-07-07 14:43:47 -0300

    Update template-default.php

--- template-default.php
+++ template-default.php
@@ -12,6 +12,7 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 section.min-vh-100.d-flex.align-items-center.spacing>.container>.row.justify-content-center.align-items-center>.col-12|c
 
+<?php apply_filters('logo_theme', array('img' => THEMEROOT . '/assets/img/logo.png')); ?>
 
 <?php 
 endwhile; endif; 
